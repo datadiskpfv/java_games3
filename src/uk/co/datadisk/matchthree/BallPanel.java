de31de127be3980f6721762a71e7d2c99ba0bf38
Paul Valle <paul.valle@datadisk.co.uk>
2017-10-21 11:59:51 +0100

    make sure the game goes not start with three balls in a roll, created methods to check for chains of 3 balls in a row

--- src/uk/co/datadisk/matchthree/BallPanel.java
+++ src/uk/co/datadisk/matchthree/BallPanel.java
@@ -95,6 +95,9 @@ public class BallPanel extends JPanel {
         for (int row = 0; row < ROWS; row++) {
             for (int col = 0; col < COLS; col++) {
                 cells[row][col] = new Cell();
+                while(twoMatchToLeft(row,col) || twoMatchAbove(row,col)){
+                    cells[row][col] = new Cell();
+                }
             }
         }
         repaint();
@@ -165,5 +168,98 @@ public class BallPanel extends JPanel {
         cells[row2][col2].copy(temp);
 
         repaint();
+    }
+
+    private boolean twoMatchAbove(int row, int col){
+        boolean match = false;
+
+        if(row > 1 && row < ROWS && col >= 0 && col < COLS){
+            int color1 = cells[row][col].getColor();
+            int color2 = cells[row-1][col].getColor();
+            int color3 = cells[row-2][col].getColor();
+
+            if(color1 == color2 && color1 == color3){
+                match = true;
+            }
+        }
+        return match;
+    }
+
+    private boolean twoMatchToLeft(int row, int col){
+        boolean match = false;
+        if (row >= 0 && row < ROWS && col > 1 && col < COLS){
+            int color1 = cells[row][col].getColor();
+            int color2 = cells[row][col-1].getColor();
+            int color3 = cells[row][col-2].getColor();
+
+            if(color1 == color2 && color1 == color3){
+                match = true;
+            }
+        }
+        return match;
+    }
+
+    private int markChainsAndGetPointsInRow(int row){
+        int points = 0;
+
+        // get the color of the first ball
+        int color = cells[row][0].getColor();
+        int count = 1;
+
+        // loop through each column of the row
+        for (int col = 1; col < COLS; col++) {
+
+            // count the balls in each color
+            int nextColor = cells[row][col].getColor();
+            if(nextColor == color){
+                count++;
+            } else {
+                color = nextColor;
+                count = 1;
+            }
+
+            // mark chains of three or more. When count
+            // reaches 3, also mark previous two.
+            if (count == 3){
+                cells[row][col].setInChain(true);
+                cells[row][col-1].setInChain(true);
+                cells[row][col-2].setInChain(true);
+            } else if (count > 3){
+                cells[row][col].setInChain(true);
+            }
+        }
+        return points;
+    }
+
+    private int markChainsAndGetPointsInCol(int col){
+        int points = 0;
+
+        // get the color of the first ball
+        int color = cells[0][col].getColor();
+        int count = 1;
+
+        // loop through each column of the row
+        for (int row = 1; row < ROWS; row++) {
+
+            // count the balls in each color
+            int nextColor = cells[row][col].getColor();
+            if(nextColor == color){
+                count++;
+            } else {
+                color = nextColor;
+                count = 1;
+            }
+
+            // mark chains of three or more. When count
+            // reaches 3, also mark previous two.
+            if (count == 3){
+                cells[row][col].setInChain(true);
+                cells[row-1][col].setInChain(true);
+                cells[row-2][col].setInChain(true);
+            } else if (count > 3){
+                cells[row][col].setInChain(true);
+            }
+        }
+        return points;
     }
 }
